d52d62e92ad681280ce19430bccde74958a5d458
Ashima Shrivastava <ashima.shrivastava@matrixres.com>
2018-05-03 16:02:09 -0400

    updated readme

--- ClientApp/app/models/gameBoard.ts
+++ ClientApp/app/models/gameBoard.ts
@@ -2,8 +2,6 @@ import { Position } from './position';
 
 
 export class Square {
-    //row: number;
-    //col: number;
-    validMove: boolean;
     position: Position;
+    validMove: boolean;
 }
--- ClientApp/app/stores/gameBoard.store.ts
+++ ClientApp/app/stores/gameBoard.store.ts
@@ -28,7 +28,6 @@ export function squares(state: State = [], action: Actions): State {
     switch (action.type) {
 
         case DISPLAY_SQUARES:
-            console.log(action.payload);
             return action.payload;
 
         case HIGHLIGHT_SQUARES:
